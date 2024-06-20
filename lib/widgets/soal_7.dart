0548c121e796fc81e9395ed791a552823422473a
audy123ari <audy123ari@gmail.com>
2024-06-20 04:32:02 +0000

    Soal 16

--- lib/widgets/soal_7.dart
+++ lib/widgets/soal_7.dart
@@ -1,89 +1,54 @@
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
-class soal_7 extends StatelessWidget {
+class soal_7 extends StatefulWidget {
   const soal_7({super.key});
+
+  @override
+  State<soal_7> createState() => _soal_7State();
+}
+
+// ignore: camel_case_types
+class _soal_7State extends State<soal_7> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Soal 12')),
+        title: const Center(child: Text('Soal 16')),
         backgroundColor: Colors.teal,
       ),
-      body: Align(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Scrollbar(
+        controller: _scrollController,
+        child: ListView.separated(
+          controller: _scrollController,
+          itemCount: 11,
+          separatorBuilder: (context, index) => const SizedBox(height: 16),
+          itemBuilder: (context, index) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Container(
                   width: 150,
                   height: 150,
-                  color: Colors.blue,
-                  child: const Center(
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  child: Center(
                     child: Text(
-                      'Hello',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 150,
-                  height: 150,
-                  color: Colors.yellow,
-                  child: const Center(
-                    child: Text(
-                      'Hello',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Spacer(),
-            const SizedBox(height: 16.0),
-            const FlutterLogo(
-              size: 100.0,
-            ),
-            const SizedBox(height: 16.0),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 150,
-                  height: 150,
-                  color: Colors.green,
-                  child: const Center(
-                    child: Text(
-                      'Hello',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 150,
-                  height: 150,
-                  color: Colors.red,
-                  child: const Center(
-                    child: Text(
-                      'Hello',
-                      style: TextStyle(
+                      'Hello $index',
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
@@ -92,8 +57,8 @@ class soal_7 extends StatelessWidget {
                   ),
                 ),
               ],
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
