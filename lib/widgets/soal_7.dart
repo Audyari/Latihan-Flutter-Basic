72e2e01e69115ab20c2ca4d12649fd3713bf416c
audy123ari <audy123ari@gmail.com>
2024-06-20 04:19:05 +0000

    Soal 15

--- lib/widgets/soal_7.dart
+++ lib/widgets/soal_7.dart
@@ -12,86 +12,88 @@ class soal_7 extends StatelessWidget {
         backgroundColor: Colors.teal,
       ),
       body: Align(
-        alignment: Alignment.topLeft,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.blue,
-                    child: const Center(
-                      child: Text(
-                        'Hello',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue,
+                  child: const Center(
+                    child: Text(
+                      'Hello',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.yellow,
-                    child: const Center(
-                      child: Text(
-                        'Hello',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.yellow,
+                  child: const Center(
+                    child: Text(
+                      'Hello',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ],
-              ),
-              const Spacer(), // Tambahkan Spacer untuk mengisi sisa ruang
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.green,
-                    child: const Center(
-                      child: Text(
-                        'Hello',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            const SizedBox(height: 16.0),
+            const FlutterLogo(
+              size: 100.0,
+            ),
+            const SizedBox(height: 16.0),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.green,
+                  child: const Center(
+                    child: Text(
+                      'Hello',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.red,
-                    child: const Center(
-                      child: Text(
-                        'Hello',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.red,
+                  child: const Center(
+                    child: Text(
+                      'Hello',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
