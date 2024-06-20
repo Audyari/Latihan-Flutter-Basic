424eacad84e7535334bf1e2558818bab42d12304
audy123ari <audy123ari@gmail.com>
2024-06-20 03:45:47 +0000

    Soal 12

--- lib/widgets/soal_7.dart
+++ lib/widgets/soal_7.dart
@@ -1,16 +1,14 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
 class soal_7 extends StatelessWidget {
-  const soal_7({
-    super.key,
-  });
+  const soal_7({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Soal 11')),
+        title: const Center(child: Text('Soal 12')),
         backgroundColor: Colors.teal,
       ),
       body: Align(
@@ -18,39 +16,81 @@ class soal_7 extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(0),
           child: Column(
-             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.blue,
-                child: const Center(
-                  child: Text(
-                    'Hello',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.blue,
+                    child: const Center(
+                      child: Text(
+                        'Hello',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(width: 16.0),
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.yellow,
+                    child: const Center(
+                      child: Text(
+                        'Hello',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-               const SizedBox(height: 16.0),
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.yellow,
-                child: const Center(
-                  child: Text(
-                    'Hello',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.green,
+                    child: const Center(
+                      child: Text(
+                        'Hello',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(width: 16.0),
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.red,
+                    child: const Center(
+                      child: Text(
+                        'Hello',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
