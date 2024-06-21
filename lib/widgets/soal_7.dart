5a8df4144c12679ec5ac380b8e417b98bf0176b0
audy123ari <audy123ari@gmail.com>
2024-06-21 07:36:57 +0000

    soal ke 20 stack center

--- lib/widgets/soal_7.dart
+++ lib/widgets/soal_7.dart
@@ -7,51 +7,69 @@ class MyHomePage1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Stack of Containers'),
+        title: const Text('Stack of Centered Containers'),
       ),
       body: Stack(
-        alignment: Alignment.topLeft,
+        alignment: Alignment.center,
         children: [
           Positioned(
-            child: Container(
-              width: 350,
-              height: 350,
-              color: Colors.orange,
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 350,
+                height: 350,
+                color: Colors.orange,
+              ),
             ),
           ),
           Positioned(
-            child: Container(
-              width: 300,
-              height: 300,
-              color: Colors.purple,
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 300,
+                height: 300,
+                color: Colors.purple,
+              ),
             ),
           ),
           Positioned(
-            child: Container(
-              width: 250,
-              height: 250,
-              color: Colors.yellow,
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 250,
+                height: 250,
+                color: Colors.yellow,
+              ),
             ),
           ),
           Positioned(
-            child: Container(
-              width: 200,
-              height: 200,
-              color: Colors.red,
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.red,
+              ),
             ),
           ),
           Positioned(
-            child: Container(
-              width: 150,
-              height: 150,
-              color: Colors.green,
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 150,
+                height: 150,
+                color: Colors.green,
+              ),
             ),
           ),
           Positioned(
-            child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.blue,
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
             ),
           ),
         ],
